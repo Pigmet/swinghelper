113872ace1dbb8a44fd3bc58107d7a5706fa0240
Gou <nakamura462645@gmail.com>
2020-04-17 13:31:30 +0900

    added html

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject swinghelp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "helper methods for seesaw."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
--- src/swinghelp/core.clj
+++ src/swinghelp/core.clj
@@ -1,5 +1,6 @@
 (ns swinghelp.core
-  (:require [defun.core :refer [defun]])
+  (:require [defun.core :refer [defun]]
+            [clojure.string :refer [join]])
   (:use [seesaw core font]))
 
 (defn get-elem-id [root id]
@@ -35,4 +36,32 @@
        (config! el component-key v )))
    root))
 
+;; html
 
+(defn- decorator [k s]
+  (let [beg (format "<%s>" (name k))
+        end (format "</%s>" (name k))]
+    (str beg s end)))
+
+(s/def ::html-spec
+  (s/or
+   :literal (complement coll?)
+   :command (s/cat :k keyword? :args (s/* ::html-spec))
+   :invalid any?))
+
+;;(eduplot.helper/ditch html)
+
+(defmulti html*
+  "Converts data to html string."
+  (fn [x] (key (s/conform ::html-spec x))))
+
+(defmethod html* :invalid [x]
+  (throw (Exception.
+          (format "html error: don't know how to convert %s." x))))
+
+(defmethod html* :literal [x] (str x))
+
+(defmethod html* :command [[k & args]]
+  (decorator k (join " " (map html* args))))
+
+(defn html [& args] (html* (list* :html args)))
